6c74d1ac8e28244bea250d71ef17777534d5026f
Jeonghyeon Park <fishbox0923@gmail.com>
2023-11-25 04:55:00 +0000

    logic complete

--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -2,12 +2,40 @@
  * express Router middleware for Schedule APIs
  *
  * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
+ * @author Jeonghyeon Park <fishbox0923@gmail.com>
  */
 
 import * as express from 'express';
-// import * as Cosmos from '@azure/cosmos';
-// import ServerConfig from '../ServerConfig';
+import * as Cosmos from '@azure/cosmos';
+import ServerConfig from '../ServerConfig';
+import ForbiddenError from '../exceptions/ForbiddenError';
+import BadRequestError from '../exceptions/BadRequestError';
+import CourseListMetaData from '../datatypes/courseListMetaData/CourseListMetaData';
 
 const scheduleRouter = express.Router();
+
+// GET: /schedule/course
+scheduleRouter.get('/course', async (req, res, next) => {
+    const dbClient: Cosmos.Database = req.app.locals.dbClient;
+
+    try {
+      // Check Origin header or application key
+      if (
+        req.header('Origin') !== req.app.get('webpageOrigin') &&
+        !req.app.get('applicationKey').includes(req.header('X-APPLICATION-KEY'))
+      ) {
+        throw new ForbiddenError();
+      }
+      
+      const termCode = req.body.termCode;
+      const courseName = req.body.courseName;
+      const courseList= await CourseListMetaData.searchCourse(dbClient, termCode, courseName);
+
+      res.status(200).json(courseList);
+    }
+    catch (e) {
+      next(e);
+    }
+});
 
 export default scheduleRouter;
