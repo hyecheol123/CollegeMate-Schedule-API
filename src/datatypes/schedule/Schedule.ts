0436e064c8e1ee90a072a707359a0831d357f63d
Jeonghyeon Park <fishbox0923@gmail.com>
2024-01-02 10:21:15 +0000

    Initalized the logic

--- src/datatypes/schedule/Schedule.ts
+++ src/datatypes/schedule/Schedule.ts
@@ -2,13 +2,14 @@
  * Define type and used CRUD methods for schedule
  *
  * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
+ * @author Jeonghyeon Park <fishbox0923@gmail.com>
  */
 
-// import * as Cosmos from '@azure/cosmos';
-// import ServerConfig from '../../ServerConfig';
+import * as Cosmos from '@azure/cosmos';
+import ServerConfig from '../../ServerConfig';
 
 // DB Container id
-// const SCHEDULE = 'schedule';
+const SCHEDULE = 'schedule';
 
 interface Event {
   id: string;
@@ -56,5 +57,33 @@ export default class Schedule {
     this.termCode = termCode;
     this.sessionList = sessionList;
     this.eventList = eventList;
+  }
+
+  /**
+   * Retrieve list of schedule associated with the user idenfied by the given email address (base64urlsafe encoded).
+   * 
+   * @param dbClient Cosmos DB client
+   * @param email base64urlsafe encoded email address
+   */
+  static async retrieveScheduleList(
+    dbClient: Cosmos.Database,
+    email: string
+  ): Promise<Schedule[]> {
+    const querySpec: Cosmos.SqlQuerySpec = {
+      query: `SELECT * FROM c WHERE c.email = @email`,
+      parameters: [
+        {
+          name: '@email',
+          value: email,
+        },
+      ],
+    };
+
+    const { resources: scheduleList } = await dbClient
+      .container(SCHEDULE)
+      .items.query(querySpec)
+      .fetchAll();
+
+    return scheduleList as Schedule[];
   }
 }
--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -2,12 +2,63 @@
  * express Router middleware for Schedule APIs
  *
  * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
+ * @author Jeonghyeon Park <fishbox0923@gmail.com>
  */
 
 import * as express from 'express';
-// import * as Cosmos from '@azure/cosmos';
-// import ServerConfig from '../ServerConfig';
+import * as Cosmos from '@azure/cosmos';
+import ServerConfig from '../ServerConfig';
+import ForbiddenError from '../exceptions/ForbiddenError';
+import UnauthenticatedError from '../exceptions/UnauthenticatedError';
+import verifyAccessToken from '../functions/JWT/verifyAccessToken';
+import NotFoundError from '../exceptions/NotFoundError';
+import { validateEmail } from '../functions/inputValidator/validateEmail';
+import Schedule from '../datatypes/schedule/Schedule';
 
 const scheduleRouter = express.Router();
+
+// GET: /schedule/{base64Email}/list
+scheduleRouter.get('/:base64Email/list', async (req, res, next) => {
+    const dbClient: Cosmos.Database = req.app.locals.dbClient;
+
+    try {
+        // Check Origin header or application key
+        if (
+          req.header('Origin') !== req.app.get('webpageOrigin') &&
+          !req.app.get('applicationKey').includes(req.header('X-APPLICATION-KEY'))
+        ) {
+          throw new ForbiddenError();
+        }
+
+        // Header check - access token
+        const accessToken = req.header('X-ACCESS-TOKEN');
+        if (accessToken === undefined) {
+        throw new UnauthenticatedError();
+        }
+        const tokenContents = verifyAccessToken(
+        accessToken,
+        req.app.get('jwtAccessKey')
+        );
+
+        // Parameter check if received
+        const requestUserEmail = Buffer.from(
+            req.params.base64Email,
+            'base64url'
+        ).toString('utf8');
+
+        if (!validateEmail(requestUserEmail)) {
+            throw new NotFoundError();
+        }
+
+        // DB Operation
+        const email = tokenContents.id;
+        const scheduleList = await Schedule.retrieveScheduleList(dbClient, email);
+
+        res.status(200).send(scheduleList);
+
+    } catch (e) {
+        next(e);
+    }
+});
 
 export default scheduleRouter;
