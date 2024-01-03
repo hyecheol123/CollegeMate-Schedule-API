8d385dee986b4c8178259b78a8ed777c3a93223e
Jeonghyeon Park <fishbox0923@gmail.com>
2024-01-03 16:07:00 +0000

    Initalized the logic except an exception

--- src/datatypes/schedule/Schedule.ts
+++ src/datatypes/schedule/Schedule.ts
@@ -2,13 +2,14 @@
  * Define type and used CRUD methods for schedule
  *
  * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
+ * @author Jeonghyeon Park <fishbox0923@gmail.com>
  */
 
-// import * as Cosmos from '@azure/cosmos';
-// import ServerConfig from '../../ServerConfig';
+import * as Cosmos from '@azure/cosmos';
+import ServerConfig from '../../ServerConfig';
 
 // DB Container id
-// const SCHEDULE = 'schedule';
+const SCHEDULE = 'schedule';
 
 interface Event {
   id: string;
@@ -56,5 +57,43 @@ export default class Schedule {
     this.termCode = termCode;
     this.sessionList = sessionList;
     this.eventList = eventList;
+  }
+  
+  /**
+   * check if the schedule with the id exists in the database
+   * 
+   * @param {Cosmos.Database} dbClient Cosmos DB Client
+   * @param {string} id schedule id
+   */
+  static async findExist(
+    dbClient: Cosmos.Database,
+    id: string,
+  ): Promise<boolean> {
+    const dbOps = await dbClient
+      .container(SCHEDULE)
+      .items.query({
+        query: `SELECT * FROM c WHERE c.id = "${id}"`,
+      })
+    .fetchAll();
+    return dbOps.resources.length !== 0;
+  }
+
+  /**
+   * find the detail of the schedule
+   *
+   * @param {Cosmos.Database} dbClient Cosmos DB Client
+   * @param {string} id schedule id
+   */
+  static async findScheduleDetail(
+    dbClient: Cosmos.Database,
+    id: string,
+  ): Promise<Schedule> {
+    const schedule = await dbClient
+      .container(SCHEDULE)
+      .items.query({
+        query: `SELECT * FROM c WHERE c.id = "${id}"`,
+      })
+    .fetchAll();
+    return schedule.resources[0];
   }
 }
--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -2,12 +2,57 @@
  * express Router middleware for Schedule APIs
  *
  * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
+ * @author Jeonghyeon Park <fishbox0923@gmail.com>
  */
 
 import * as express from 'express';
-// import * as Cosmos from '@azure/cosmos';
-// import ServerConfig from '../ServerConfig';
+import * as Cosmos from '@azure/cosmos';
+import ServerConfig from '../ServerConfig';
+import ForbiddenError from '../exceptions/ForbiddenError';
+import UnauthenticatedError from '../exceptions/UnauthenticatedError';
+import verifyAccessToken from '../functions/JWT/verifyAccessToken';
+import Schedule from '../datatypes/schedule/Schedule';
+import NotFoundError from '../exceptions/NotFoundError';
 
 const scheduleRouter = express.Router();
+
+// GET: /schedule/{scheduleId}
+scheduleRouter.get('/:scheduleId', async (req, res, next) => {
+    const dbClient: Cosmos.Database = req.app.locals.dbClient;
+
+    try {
+      // Check Origin header or application key
+      if (
+        req.header('Origin') !== req.app.get('webpageOrigin') &&
+        !req.app.get('applicationKey').includes(req.header('X-APPLICATION-KEY'))
+      ) {
+        throw new ForbiddenError();
+      }
+
+      // Header check - access token
+      const accessToken = req.header('X-ACCESS-TOKEN');
+      if (accessToken === undefined) {
+        throw new UnauthenticatedError();
+      }
+
+      // const tokenContents = verifyAccessToken(
+      //   accessToken,
+      //   req.app.get('jwtAccessKey')
+      // );
+      // const email = tokenContents.id;
+      const scheduleId = req.params.scheduleId;
+
+      if (!await Schedule.findExist(dbClient, scheduleId)) {
+        throw new NotFoundError();
+      }
+
+      const scheduleDetail = await Schedule.findScheduleDetail(dbClient, scheduleId);
+
+      // Response
+      res.status(200).send(scheduleDetail);
+    } catch (e) {
+        next(e);
+    }
+});
 
 export default scheduleRouter;
